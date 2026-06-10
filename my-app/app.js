8f1a41ff51ca3f91cf2fd36eda93b7cf5f09a63b
Akshara <devopsakshara@gmail.com>
2026-06-10 10:28:29 +0000

    testing for error

--- my-app/app.js
+++ my-app/app.js
@@ -1,7 +1,8 @@
+throw new Error("test failure");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Akshara.. I'm testing CI/CD live.. Yeah it worked.. Never make changes in EC2. Only on local machine is allowed..Now i'm adding production environment and before that a simple test to see if my app runs or not");
+  res.end("Hello from Akshara.. I'm testing CI/CD live.. Yeah it worked.. Never make changes in EC2. Only on local machine is allowed..Now i'm adding production environment and before that a simple test to see if my app runs or not.. now am checking if the content goes live if any error is found in testing stage");
 });
 
 server.listen(3000, () => {
