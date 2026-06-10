f88a05cf63270bdfe4d90378dc06dd8d2a784172
Akshara <devopsakshara@gmail.com>
2026-06-10 10:24:54 +0000

    changed app.js again to see if it works with the yml file i added

--- my-app/app.js
+++ my-app/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Akshara.. I'm testing CI/CD live.. Yeah it worked.. Never make changes in EC2. Only on local machine is allowed");
+  res.end("Hello from Akshara.. I'm testing CI/CD live.. Yeah it worked.. Never make changes in EC2. Only on local machine is allowed..Now i'm adding production environment and before that a simple test to see if my app runs or not");
 });
 
 server.listen(3000, () => {
