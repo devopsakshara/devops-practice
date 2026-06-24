74c4f58aab79e8ab42facd82ebe8ac4e72ac2627
Akshara <devopsakshara@gmail.com>
2026-06-24 11:03:36 +0000

    dynamic port on pythonapp.py

--- my-app/pythonapp.py
+++ my-app/pythonapp.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +7,5 @@ def home():
     return "Hello from Dockerized Python App!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
